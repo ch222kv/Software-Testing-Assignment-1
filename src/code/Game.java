cff89aa27306ec11dae1e1d1d594b1e7b16e1062
Chrille <christoffer.holmgren@gmail.com>
2015-11-11 19:55:34 +0100

    GREEN

---
+++ src/code/Game.java
@@ -0,0 +1,17 @@
+package code;
+
+public class Game {
+	private int sticks = 20;
+
+	public Object getSticksLeft() {
+		// TODO Auto-generated method stub
+		return sticks;
+	}
+	
+	public boolean takeSticks(int i) {
+		sticks -= i;
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
