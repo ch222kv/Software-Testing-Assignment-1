d64a8cd2c7ac23bea7dc70acad43655a8a71980b
Chrille <christoffer.holmgren@gmail.com>
2015-11-17 19:07:04 +0100

    Refactoring the testsRefactoring the tests

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -7,26 +7,44 @@ import static org.mockito.Mockito.*;
 import code.*;
 
 public class Test {
-
+	private Game game = null;
+	
+	@org.junit.Before
+	public void setUp(){
+		this.game = new Game();
+	}
 	@org.junit.Test
-	public void test() {		
-		Game game = new Game();
+	public void test() {
 		
-		assertEquals(game.getSticksLeft(), 20);
-		assertTrue(game.takeSticks(1));
-		assertEquals(game.getSticksLeft(), 19);
+	}
+	@org.junit.Test
+	public void testGetSticks() {
+		assertEquals(this.game.getSticksLeft(), 20);
+		assertTrue(this.game.takeSticks(1));
+		assertEquals(this.game.getSticksLeft(), 19);
 		
+		assertFalse(this.game.takeSticks(this.game.getSticksLeft() + 1));
 		//Can't take more sticks than there are left
-		assertFalse(game.takeSticks(20));
-		for(int i = 20; i < 100; i++){
-			assertFalse(game.takeSticks(i));
-		}
-		assertFalse(game.hasEnded());
-		game.takeSticks(game.getSticksLeft());
-		assertTrue(game.hasEnded());
 		
-		game.beginGame();
-		assertFalse(game.hasEnded());
+		for(int i = 20; i < 100; i++){
+			assertFalse(this.game.takeSticks(i));
+		}
+	}
+	@org.junit.Test
+	public void testGameHasEnded(){
+		assertFalse(this.game.hasEnded());
+		this.game.takeSticks(game.getSticksLeft());
+		assertTrue(this.game.hasEnded());
+	}
+	@org.junit.Test
+	public void testBeginGame(){
+		this.game.takeSticks(this.game.getSticksLeft());
+		
+		assertTrue(this.game.hasEnded());
+		
+		this.game.beginGame();
+		
+		assertFalse(this.game.hasEnded());
 	}
 
 }
