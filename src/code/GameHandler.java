f3beb2e53b0d43ad0e4834bbd2a552e46f666b6f
Chrille <christoffer.holmgren@gmail.com>
2015-11-22 13:40:49 +0100

    RED and GREEN

--- src/code/GameHandler.java
+++ src/code/GameHandler.java
@@ -9,20 +9,26 @@ public class GameHandler {
 		this.game = game;
 	}
 
-	public void beginGame() {
+	public void beginGame() throws GameHasEndedException, GameHasNotBegunException {
 		view.displayStartMessage();
+		
 		if(view.getInput() == 'y'){
 			beginLoop();
 		}
 		view.displayQuitMessage();
 	}
-	public void beginLoop(){
+	public void beginLoop() throws GameHasEndedException, GameHasNotBegunException{
 		char input;
 		do{
 			view.getStickInput();
 			input = view.getInput();
 			if(input == 'R'){
 				game.resetGame();
+			} else if(Character.isDigit(input)){
+				try {
+					game.takeSticks(Integer.parseInt(Character.toString(input)));
+				} catch (NumberFormatException e) {
+				}
 			}
 		}while(input != 'Q');
 	}
